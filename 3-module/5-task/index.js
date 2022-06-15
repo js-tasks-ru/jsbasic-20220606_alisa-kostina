8b116d6e2cd072f28af907522df08fb4fefe7693
Alisa Kostina <asilaaniholb@icloud.com>
2022-06-15 21:54:46 +0300

    3-5 решение

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+  let numbers = [];
+  for (let key of arr) {
+    key = Number(key);
+    if (typeof key == 'number' && !isNaN(key)) {
+      numbers.push(key)
+    }
+  }
+  let result = {};
+  result.min = Math.min(...numbers);
+  result.max = Math.max(...numbers);
+  return result;
 }
