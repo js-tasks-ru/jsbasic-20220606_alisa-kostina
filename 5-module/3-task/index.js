e1de5eb7b010018cfce9a371770c67bb8c9b50a5
Alisa Kostina <asilaaniholb@icloud.com>
2022-06-29 14:15:54 +0300

    5-3 решение

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
-}
+  let arrows = document.querySelector('.carousel');
+  let arrayArrows = document.querySelectorAll('.carousel__arrow');
+  let right = arrayArrows[0];
+  let left = arrayArrows[1];
+  let imgRight = right.childNodes[1];
+  let imgLeft = left.childNodes[1];
+  let images = document.querySelector('.carousel__inner');
+  let arrayImg = document.querySelectorAll('.carousel__slide');
+  let imgWidth = arrayImg[0].offsetWidth;
+  let imgIndex = 0;
+  left.style.display = 'none';
+
+  arrows.addEventListener('click', function (event) {
+    let target = event.target;
+    if (target === right || target === imgRight) {
+      imgIndex++;
+      images.style.transform = `translateX(-${imgIndex * imgWidth}px)`
+    } else if (target === left || target === imgLeft) {
+        imgIndex--;
+        images.style.transform = `translateX(-${imgIndex * imgWidth}px)`
+    } else {
+      return;
+    }
+   
+    if (imgIndex >= 3) {
+      right.style.display = 'none';
+    } else if (imgIndex == 0) {
+      left.style.display = 'none';
+    } else {
+      left.style.display = '';
+      right.style.display = '';
+    }});
+  }
